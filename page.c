b01a7fbc7ae7888cc2956893b21dc64e8bca3dfd
ymt <hfytgc@gmail.com>
2020-04-28 15:07:34 +0900

    Add functions page_set_int and page_get_int.

--- page.c
+++ page.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include "page.h"
 
+// 参照 https://github.com/hawstan/ByteBuffer/blob/master/ByteBuffer.c
+
 Page* new_page_blksize(unsigned int blksize) {
     Page* page = malloc(sizeof(Page));
     if (page == NULL) {
@@ -20,4 +22,27 @@ Page* new_page_blksize(unsigned int blksize) {
 
 void page_clear(Page *page) {
     memset(page->data, 0, page->size);
+}
+
+// get 32bit(4byte) int from page.
+int page_get_int(Page *page, int offset, int* value) {
+    if (page == NULL) {
+        return 0;
+    }
+
+    // 微妙
+    *value = *((int*)(page->data + offset));
+    return 4;
+}
+
+int page_set_int(Page *page, int offset, int n) {
+    if (page == NULL) {
+        return 0;
+    }
+
+    page->data[offset++] = n & 255;
+    page->data[offset++] = (n << 8) & 255;
+    page->data[offset++] = (n << 16) & 255;
+    page->data[offset++] = (n << 24) & 255;
+    return 4;
 }
