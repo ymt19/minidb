f53d9575fa7f045ce77ead2c073609dfa2e11200
ymt <hfytgc@gmail.com>
2020-04-29 22:11:07 +0900

    Add function prototype to page.h.

--- page.h
+++ page.h
@@ -7,4 +7,13 @@ struct Page {
     unsigned char* data;
 };
 
+Page* new_page_blksize(unsigned int);
+void page_clear(Page*);
+int page_get_int(Page*, int, int*);
+int page_set_int(Page*, int, int);
+int page_get_bytes(Page*, int, void*);
+int page_set_bytes(Page*, int, void*);
+int page_get_string(Page*, int, char**);
+int page_set_string(Page*, int, char*);
+
 #endif
