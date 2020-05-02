98b0ab72bda3cb7e2523d731ef006c1b1f898d36
ymt <hfytgc@gmail.com>
2020-05-02 13:28:44 +0900

    Fix test code

--- test_file_manager.c
+++ test_file_manager.c
@@ -9,23 +9,23 @@ int main(void) {
 
     // Write a string and a number to the page and write it to the block.
     Block *blk = new_block("tmp1", 2);
-    Page *page1 = new_page_blksize();
+    Page *page1 = new_page();
     int pos1 = 88;
-    int size = page_set_string(page1, pos1, "abcdefghijklmn");
+    int size = set_string_to_page(page1, pos1, "abcdefghijklmn");
     printf("%d\n", size);
     int pos2 = pos1 + size;
-    page_set_int(page1, pos2, 1111);
+    set_int_to_page(page1, pos2, 1111);
     write_page_to_blk(blk, page1);
 
     // Read a string and a number from the block.
-    Page *page2 = new_page_blksize();
+    Page *page2 = new_page();
     read_page_from_blk(blk, page2);
     char str[MAX_STRING_SIZE];
     printf("%p main str\n", &str);
-    page_get_string(page2, pos1, str);
+    get_string_from_page(page2, pos1, str);
     printf("%s\n", str);
     int num;
-    page_get_int(page2, pos2, &num);
+    get_int_from_page(page2, pos2, &num);
     printf("%d\n", num);
     return 0;
 }
