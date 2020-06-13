de5a2494294e65835cc424a0bab1c8f1e2dc9780
ymt <hfytgc@gmail.com>
2020-06-13 16:22:14 +0900

    improve code

--- block.h
+++ block.h
@@ -2,11 +2,10 @@
 
 #define MAX_FILENAME (256)
 
-typedef struct Block Block;
-struct Block {
+typedef struct {
     char filename[MAX_FILENAME];
     int blk_number;
-};
+} Block;
 
 Block* new_block(char*, int);
 int is_equal_block(Block*, Block*);
