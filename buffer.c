a255f72207c2f90b2b909b6279fecc1b1cba8188
ymt <hfytgc@gmail.com>
2020-06-14 01:06:02 +0900

    Add buffer functions

--- buffer.c
+++ buffer.c
@@ -1,25 +1,48 @@
 #include <stdlib.h>
-
 #include "buffer.h"
 
-Buufer* new_Buffer(LogManager *lm) {
+Buffer* new_Buffer(FileManager *fm, LogManager *lm) {
     Buffer *buffer;
     buffer = malloc(sizeof(Buffer));
     if (buffer == NULL){
         return NULL;
     }
 
+    buffer->fm = fm;
     buffer->lm = lm;
-    buffer->contents = new_page();
+    buffer->page = new_page(fm->blksize);
     buffer->blk = NULL;
     buffer->pins = 0;
     buffer->txnum = -1;
     buffer->lsn = -1;
-
     return Buffer;
 }
 
-void buffer_modified(Buffer* buffer, int txnum, int lsn) {
+void buffer_modified(Buffer *buffer, int txnum, int lsn) {
     buffer->txnum = txnum;
     if (lsn >= 0) buffer->lsn = lsn;
+}
+
+int buffer_is_pinned(Buffer *buffer) {
+    if (buffer->pins > 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void buffer_flush(Buffer *buffer) {
+    if (buffer->txnum >= 0) {
+        log_flush_to_lsn(buffer->lm, buffer->lsn);
+        fm_write(buffer->fm, buffer->blk, buffer->page);
+        buffer->txnum = -1;
+    }
+}
+
+void buffer_pin(Buffer *buffer) {
+    buffer->pins++;
+}
+
+void buffer_unpin(Buffer *buffer) {
+    buffer->pins--;
 }
--- buffer.h
+++ buffer.h
@@ -3,15 +3,19 @@
 #include "file_manager.h"
 #include "log_manager.h"
 
-typedef struct Buffer Buffer;
-struct Buffer {
+typedef struct {
+    FileManager *fm;
     LogManager *lm;
-    Page *contents;
+    Page *page;
     Block *blk;
     int pins;
     int txnum;
     int lsn;
-}
+} Buffer;
 
-Buffer* new_Buffer(LogManager*);
+Buffer* new_Buffer(FileManager*, LogManager*);
 void buffer_modified(Buffer*, int, int);
+int buffer_is_pinned(Buffer*);
+void buffer_flush(Buffer*);
+void buffer_pin(Buffer*);
+void buffer_unpin(Buffer*);
