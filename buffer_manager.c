7c47f96a4313151cbc44cf51b12037f477ef0a6d
ymt <hfytgc@gmail.com>
2020-09-05 17:10:09 +0900

    add: BufferPoolのBufferをpinする関数

--- buffer_manager.c
+++ buffer_manager.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
+#include <time.h>
 #include "buffer_manager.h"
+
+static int waiting_too_long(BufferManager*, struct timespec);
+static Buffer* bm_try_to_pin(BufferManager*, Block*);
+static Buffer* bm_find_same_buffer(BufferManager*, Block*);
+static Buffer* bm_find_unpinned_buffer(BufferManager*);
 
 
 BufferManager *new_BufferManager(FileManager *fm, LogManager *lm, int num_buffs, int time_limit) {
@@ -16,7 +22,7 @@ BufferManager *new_BufferManager(FileManager *fm, LogManager *lm, int num_buffs,
     }
 
     bm->available_buffs = num_buffs;
-    bm->max_wating_time = 10000000;   // 10sec
+    bm->max_wating_time = 10;           // 10sec
     return bm;
 }
 
@@ -37,10 +43,66 @@ void bm_unpin(BufferManager *bm, Buffer *buff) {
 }
 
 /**
- * 
+ * BufferPoolのBufferを指定のBlockでpinする。
+ * 失敗するとNULL
  */
-void bm_pin(BufferManager *bm, Block *blk) {
+Buffer* bm_pin(BufferManager *bm, Block *blk) {
+    Buffer buff;
+    struct timespec start_time;
 
+    // 現在時間の測定
+    clock_gettime(CLOCK_REALTIME, &start_time);
+
+    buff = bm_try_to_pin(bm, blk);
+    while (buff == NULL && waiting_too_long(bm, start_time)) {
+        buff = bm_try_to_pin(bm, blk);
+    }
+
+    return NULL;
+}
+
+/**
+ * 時間制限の確認をする。
+ */
+int waiting_too_long(BufferManager *bm, struct timespec start_time) {
+    struct timespec end_time;
+    unsigned int waiting_time;
+
+    // 現在時間の測定
+    clock_gettime(CLOCK_REALTIME, &end_time);
+
+    // 秒単位による待ち時間の計測
+    waiting_time = (unsigned int)(end_time.tv_sec - start_time.tv_sec);
+
+    if (wating_time > bm->max_wating_time) {
+        return 1;
+    }
+    return 0;
+}
+
+/**
+ * BufferPoolからBlockを割り当てるBufferを探す。
+ */
+Buffer* bm_try_to_pin(BufferManager *bm, Block *blk) {
+    Buffer *buff;
+
+    // 同じblockを持つbufferがあるか確認する
+    buff = bm_find_same_buffer(bm, blk);
+
+    // BufferPoolからunpinnedなbufferを探す
+    if (buff == NULL) {
+        buff = bm_find_unpinned_buffer(bm);
+        if (buff == NULL)
+            return NULL;
+        buffer_assign_to_block(buff, blk);
+    }
+
+    if (!buffer_is_pinned(buff))
+        bm->available_buffs--;
+
+    buffer_pin(buff);
+
+    return buff;       
 }
 
 /**
--- buffer_manager.h
+++ buffer_manager.h
@@ -5,9 +5,10 @@
 typedef struct {
     Buffer *buffer_pool;
     int available_buffs;
-    int max_wating_time;       // second
+    unsigned int max_wating_time;       // 秒単位
 } BufferManager;
 
 BufferManager *new_BufferManager(FileManager*, LogManager*, int);
 void bm_flush_all(int);
 void bm_unpin(BufferManager*, Buffer*);
+Buffer* bm_pin(BufferManager*, Block*)
