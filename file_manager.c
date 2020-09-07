e224ee6c70b24b49e41f26b708ce9303ad778be8
ymt <hfytgc@gmail.com>
2020-09-07 23:55:13 +0900

    fm_readとfm_writeの仕様の変更

--- file_manager.c
+++ file_manager.c
@@ -15,8 +15,8 @@ static unsigned char checksum(unsigned char*, int);
  * 
  * pathname     : ディレクトリ名
  * data_size    : Blockのデータの大きさ
- * @return 成功したら、FileManagerのポインタ
- * @return 失敗したら、NULL
+ * @return checksum一致なら、FileManagerのポインタ
+ * @return checksum不一致なら、NULL
  */
 FileManager* new_FileManager(char *pathname, unsigned int data_size) {
     FileManager *fm;
@@ -48,8 +48,6 @@ FileManager* new_FileManager(char *pathname, unsigned int data_size) {
 /**
  * 指定のBlockの情報を指定のPageに読み込む。
  * checksumを元に誤り検知を行う。
- * 読み込みを10回行い成功しない場合、失敗とする。
- * <-----Data-----><--checksum-->
  * 
  * fm   : FileManager
  * blk  : 読み込み元のBlock情報
@@ -58,7 +56,7 @@ FileManager* new_FileManager(char *pathname, unsigned int data_size) {
  * @return 失敗したら、0
  */
 int fm_read(FileManager *fm, Block *blk, Page *page) {
-    int i, fd;
+    int fd;
     unsigned char *bytes;   // Blockの全データを格納
     unsigned char chsum;    // チェックサム
 
@@ -77,26 +75,24 @@ int fm_read(FileManager *fm, Block *blk, Page *page) {
         exit(1);
     }
 
-    // 10回まで読み込みを行う
-    for (i = 0; i < 10; i++) {
-        if (read(fd, bytes, fm->blk_size) == -1) {
-            perror("read");
-            exit(1);
-        }
-
-        // Blockの末尾にあるchecksumの値を抽出する
-        memcpy(&chsum, bytes + fm->data_size, sizeof(unsigned char));
-
-        if (chsum == checksum(bytes, fm->data_size)) {
-            // 読み込み成功
-            memcpy(page->data, bytes, fm->data_size);
-            free(bytes);
-            close(fd);
-            return 1;
-        }
+    // read変更 blk_size分
+    if (read(fd, bytes, fm->blk_size) == -1) {
+        perror("read");
+        exit(1);
     }
 
-    // 読み込み失敗
+    // Blockの末尾にあるchecksumの値を抽出する
+    memcpy(&chsum, bytes + fm->data_size, sizeof(unsigned char));
+
+    if (chsum == checksum(bytes, fm->data_size)) {
+        // checksum一致
+        memcpy(page->data, bytes, fm->data_size);
+        free(bytes);
+        close(fd);
+        return 1;
+    }
+
+    // checksum不一致
     free(bytes);
     close(fd);
     return 0;
@@ -105,77 +101,38 @@ int fm_read(FileManager *fm, Block *blk, Page *page) {
 /**
  * 指定のBlockに指定のPageの情報を書き込む。
  * checksumを元に誤り検知を行う。
- * 書き込みを10回行い、そのBlockをchecksumを元に
- * 読み込みが成功すれば、書き込みを成功にする。
- * <-----Data-----><--checksum-->
  * 
  * fm   : FileManager
  * blk  : 書き込み先のBlock情報
  * page : 書き込み元のPage
- * @return 成功したら、1
- * @return 失敗したら、0
  */
-int fm_write(FileManager *fm, Block *blk, Page *page) {
-    int i, fd;
-    unsigned char *bytes;       // Blockのデータを格納
-    unsigned char wr_chsum;     // 書き込みをしたデータに付けたチェックサム
-    unsigned char rd_chsum;     // 読み込みをしたデータに付いていたチェックサム
+void fm_write(FileManager *fm, Block *blk, Page *page) {
+    int fd;
+    unsigned char chsum;     // 書き込みをしたデータに付けたチェックサム
 
-    if ((bytes = malloc(sizeof(unsigned char) * fm->blk_size)) == NULL) {
-        perror("malloc");
-        return 0;
-    }
-
-    if ((fd = open(blk->filename, O_RDWR | O_CREAT, 0777)) == -1) {
+    if ((fd = open(blk->filename, O_WRONLY | O_CREAT, 0777)) == -1) {
         perror("open");
         exit(1);
     }
-
-    // 10回まで書き込みを行う
-    for (i = 0; i < 10; i++) {
-        /*********書き込み*********/
-        if (lseek(fd, blk->blk_number * fm->blk_size, SEEK_SET) == -1) {
-            perror("lseek");
-            exit(1);
-        }
-        // データ部への書き込み
-        if (write(fd, page->data, fm->data_size) == -1) {
-            perror("write");
-            exit(1);
-        }
-        // チェックサム値の書き込み
-        wr_chsum = checksum(page->data, fm->data_size);
-        if (write(fd, &wr_chsum, fm->checksum_size) == -1) {
-            perror("write");
-            exit(1);
-        }
-        fsync(fd);
-
-        /*********読み込みによる誤り検知*********/
-        if (lseek(fd, blk->blk_number * fm->blk_size, SEEK_SET) == -1) {
-            perror("lseek");
-            exit(1);
-        }
-        if (read(fd, bytes, fm->blk_size) == -1) {
-            perror("read");
-            exit(1);
-        }
-
-        // Blockの末尾にあるchecksumの値を抽出する
-        memcpy(&rd_chsum, bytes + fm->data_size, sizeof(unsigned char));
-
-        // 書き込みしたデータについて
-        // 正常に読み取りが行えたら、書き込み成功
-        if (rd_chsum == checksum(bytes, fm->data_size)) {
-            free(bytes);
-            close(fd);
-            return 1;
-        }
+    if (lseek(fd, blk->blk_number * fm->blk_size, SEEK_SET) == -1) {
+        perror("lseek");
+        exit(1);
+    }
+    // データ部への書き込み
+    // writeAllの追加
+    if (write(fd, page->data, fm->data_size) == -1) {
+        perror("write");
+        exit(1);
+    }
+    // チェックサム値の書き込み
+    chsum = checksum(page->data, fm->data_size);
+    if (write(fd, &chsum, fm->checksum_size) == -1) {
+        perror("write");
+        exit(1);
     }
 
-    free(bytes);
+    fsync(fd);
     close(fd);
-    return 0;
 }
 
 /**
--- file_manager.h
+++ file_manager.h
@@ -3,6 +3,15 @@
 #include "block.h"
 #include "page.h"
 
+/**
+ * Blockは、データとchecksumで構成される。
+ * <-----data_size-----><--checksum_size-->
+ * <--------------blk_size---------------->
+ * 
+ * data_size    : データのみのサイズ
+ * checksum_size: checksumのサイズ
+ * blk_size     : data_size + checksum_size
+ */
 typedef struct {
     int data_size;          // データのみのサイズ
     int checksum_size;      // checksum用のサイズ
@@ -11,6 +20,6 @@ typedef struct {
 
 FileManager* new_FileManager(char*, unsigned int);
 int fm_read(FileManager*, Block*, Page*);
-int fm_write(FileManager*, Block*, Page*);
+void fm_write(FileManager*, Block*, Page*);
 Block* fm_append_newblk(FileManager*, char*);
 int fm_file_size(FileManager*, char*);
