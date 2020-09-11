778eb63da67aca74a3fcde59e0d20617bb9ebebd
ymt <hfytgc@gmail.com>
2020-09-11 20:04:05 +0900

    コメントの追加

--- buffer.c
+++ buffer.c
@@ -1,30 +1,69 @@
 #include <stdlib.h>
 #include "buffer.h"
 
-Buffer *new_Buffer(FileManager *fm, LogManager *lm) {
-    Buffer *buffer;
-    buffer = malloc(sizeof(Buffer));
-    if (buffer == NULL){
+/**
+ * @brief   構造体Bufferを指定した数のメモリ領域を確保する。
+ * @param   (fm) FileManager
+ * @param   (lm) LogManager
+ * @param   (num_buffs) 確保するBufferの個数
+ * @return  成功したら、確保したメモリ領域
+ * @return  失敗したら、NULL
+ * @note
+ * 指定した個数のBufferを連続するメモリ領域に確保される。
+ * @attention
+ * 呼び出した関数は、指定した個数を記録しておく必要がある。
+ */
+Buffer *new_Buffers(FileManager *fm, LogManager *lm, int num_buffs) {
+    int i;
+    Buffer *buffs;
+
+    buffs = malloc(sizeof(Buffer) * num_buffs);
+    if (buffs == NULL){
         return NULL;
     }
 
-    buffer->fm = fm;
-    buffer->lm = lm;
-    buffer->page = new_page(fm->blksize);
-    buffer->blk = NULL;
-    buffer->pins = 0;
-    buffer->txnum = -1;
-    buffer->lsn = -1;
-    return Buffer;
+    for (i = 0; i < num_buffs; i++) {
+        buffs[i]->fm = fm;
+        buffs[i]->lm = lm;
+        buffs[i]->page = new_page(fm->blksize);
+        buffs[i]->blk = NULL;
+        buffs[i]->pins = 0;
+        buffs[i]->txnum = -1;
+        buffs[i]->lsn = -1;
+    }
+    return buffs;
 }
 
-void buffer_modified(Buffer *buffer, int txnum, int lsn) {
-    buffer->txnum = txnum;
-    if (lsn >= 0) buffer->lsn = lsn;
+/**
+ * @brief   BufferのPageの内容を変更したときの、Bufferの情報の
+ *          更新をする
+ * @param   (buff) Buffer
+ * @param   (txnum) BufferのPageを変更したtxnum
+ * @param   (lsn) BufferのPageを変更したときに作成されるLSN
+ * @return
+ * @note
+ * BufferのPageの内容を変更したとき、log recordを作成し、Bufferの
+ * 情報を更新する必要がある。この関数は、そのBufferの情報を更新
+ * するものである。
+ * @attention
+ */
+void buffer_modified(Buffer *buff, int txnum, int lsn) {
+    buff->txnum = txnum;
+    buff->lsn = lsn;
+    // if (lsn >= 0) buff->lsn = lsn;
 }
 
-int buffer_is_pinned(Buffer *buffer) {
-    if (buffer->pins > 0) {
+/**
+ * @brief   Bufferがpin状態であるかを確認する
+ * @param   (buff) Buffer
+ * @return  pin状態であれば、1
+ * @return  pin状態でないならば、0
+ * @note
+ * 
+ * @attention
+ */
+int buffer_is_pinned(Buffer *buff) {
+    if (buff->pins > 0) {
         return 1;
     }
 
@@ -32,27 +71,74 @@ int buffer_is_pinned(Buffer *buffer) {
 }
 
 /**
- * BufferにBlockを割り当てる。
+ * @brief   BufferにBlockを割り当てる
+ * @param   (buff) 割り当てられるBuffer
+ * @param   (blk) 割り当てるBlock
+ * @return
+ * @note
+ * BufferにBlockが割り当てられる前に、そのBufferをflushすることで
+ * 以前のBufferの情報はfileに保存される。
+ * @attention
  */
 void buffer_assign_to_block(Buffer *buff, Block *blk) {
+    // 以前のBufferの情報をfileに保存される
     buffer_flush(buff);
+
+    // Blockを割り当てる
     buff->blk = blk;
+
+    // BufferとなるPageに割り当てるBlockを読み込む
     fm_read(buff->fm, buff->blk, buff->page);
-    buff->pins = 0;
+
+    buffer->pins = 0;
 }
 
-void buffer_flush(Buffer *buffer) {
-    if (buffer->txnum >= 0) {
-        log_flush_to_lsn(buffer->lm, buffer->lsn);
-        fm_write(buffer->fm, buffer->blk, buffer->page);
-        buffer->txnum = -1;
+/**
+ * @brief   Bufferの内容をfileにflushする
+ * @param   (buff) flushされるBuffer
+ * @return
+ * @note
+ * txnumを確認し、bufferの内容がtxによって変更されている場合のみ、
+ * bufferをflushする、つまり、bufferが管理するPageをファイルに書き
+ * 込む。
+ * @attention
+ * bufferのlsnが更新されている必要があるため、buffer_modified()を
+ * この関数の前に呼び出す必要がある。
+ */
+void buffer_flush(Buffer *buff) {
+    // bufferの内容がtxによって変更されている場合
+    if (buff->txnum >= 0) {
+        // lsnまでのlogをlog fileに書き込む
+        lm_flush_log_to_lsn(buff->lm, buff->lsn);
+
+        // bufferに相当するPageをfileに書き込む
+        fm_write(buff->fm, buff->blk, buff->page);
+
+        // txnumを初期化する
+        buff->txnum = -1;
     }
 }
 
+/**
+ * @brief   Bufferをpin状態にする
+ * @param   (buffer) pin状態にするBuffer
+ * @return
+ * @note
+ * 
+ * @attention
+ */
 void buffer_pin(Buffer *buffer) {
     buffer->pins++;
 }
 
+/**
+ * @brief   Bufferをunpin状態にする
+ * @param   (buffer) unpin状態にするBuffer
+ * @return
+ * @note
+ * 
+ * @attention
+ */
 void buffer_unpin(Buffer *buffer) {
     buffer->pins--;
 }
--- buffer.h
+++ buffer.h
@@ -3,17 +3,36 @@
 #include "file_manager.h"
 #include "log_manager.h"
 
+/**
+ * @struct  Buffer
+ * @brief   Buffer構造体
+ * @note
+ */
 typedef struct {
+    // Bufferを管理するFileManager
     FileManager *fm;
+
+    // Bufferを管理するLogManager
     LogManager *lm;
+
+    // Bufferとして扱うPage
     Page *page;
+
+    // Bufferとして扱うPageに相当するBlock
     Block *blk;
+
+    // pinされている数
     int pins;
+
+    // 変更したトランザクションのtxnum
+    // -1の場合は、未変更
     int txnum;
+
+    // Bufferが変更されたら更新されるLSN
     int lsn;
 } Buffer;
 
-Buffer* new_Buffer(FileManager*, LogManager*);
+Buffer* new_Buffers(FileManager*, LogManager*, int);
 void buffer_modified(Buffer*, int, int);
 int buffer_is_pinned(Buffer*);
 void buffer_assign_to_block(Buffer *buff, Block *blk)
