3093a50d96526304e9617727b715f09863b1d11b
ymt <hfytgc@gmail.com>
2020-09-16 18:50:23 +0900

    recovery_managerの追加

---
+++ recovery_manager.c
@@ -0,0 +1,61 @@
+#include "recovery_manager.h"
+
+/**
+ * @brief   start log recordを発行する
+ * @param
+ * @return
+ * @note
+ * 
+ * @attention
+ */
+void rm_start() {
+
+}
+
+/**
+ * @brief   commit log recordを発行する
+ * @param
+ * @return
+ * @note
+ * 
+ * @attention
+ */
+void rm_commit() {
+
+}
+
+/**
+ * @brief   rollback log recordを発行する
+ * @param
+ * @return
+ * @note
+ * 
+ * @attention
+ */
+void rm_rollback() {
+
+}
+
+/**
+ * @brief   update(int) log recordを発行する
+ * @param
+ * @return
+ * @note
+ * 
+ * @attention
+ */
+void rm_update_set_int() {
+
+}
+
+/**
+ * @brief   update(string) log recordを発行する
+ * @param
+ * @return
+ * @note
+ * 
+ * @attention
+ */
+void rm_update_set_string() {
+
+}
